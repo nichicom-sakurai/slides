c920487dc128de522fda1c03e693d42039d240fb
Yuki Sakurai <sakurai.yuki@nck.co.jp>
2025-04-29 17:06:57 +0900

    feat: pdfエクスポート機能を追加

--- main.js
+++ main.js
@@ -10,5 +10,19 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 
 const deck = new Reveal({
   plugins: [Markdown],
+  // PDF出力の設定
+  pdfSeparateFragments: true, // フラグメントを別ページに分ける
+  pdfMaxPagesPerSlide: 1, // 1スライドあたりの最大ページ数
+  showNotes: 'separate-page', // スピーカーノートを別ページに表示
 })
+
+// PDFエクスポート用のスタイルシートを追加
+if (window.location.search.match(/print-pdf/gi)) {
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = 'node_modules/reveal.js/css/print/pdf.css'
+  document.head.appendChild(link)
+}
+
 deck.initialize()
